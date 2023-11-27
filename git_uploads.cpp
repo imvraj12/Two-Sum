ff85c58417d34116cda05ca2ad4d91ec0ae6c57d
Vanshraj <114281412+imvraj12@users.noreply.github.com>
2023-11-27 12:28:55 +0530

    Add files via upload

---
+++ git_uploads.cpp
@@ -0,0 +1,44 @@
+// 1. Two Sum.
+/*
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+****
+through vector..
+
+ class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+****
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+    	cin>>a[i];
+	}
+    int target;
+    cin>>target;
+    for(int i=0;i<n-1;i++)
+    for(int j=i+1;j<n;j++)
+    {
+        if(a[i]+a[j]==target)
+        {
+            cout<<"["<<i<<","<<j
+			<<"]";
+        }
+        else
+        {
+        	j++;
+		}
+    }
+}
+
